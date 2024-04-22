4e6a259f39a5c77a0a041b2c8e7778f3ce7b8849
Milica <marinkovicm240@gmail.com>
2024-04-22 14:06:07 +0200

    Uklonjeno popunjavanje tabela

--- rest-example01/src/main/resources/liquibase/mysql/setup/dml/init-scientific-field.sql
+++
@@ -1,5 +0,0 @@
-insert into tbl_scientific_field(scf_field) values ("physical sciences");
-insert into tbl_scientific_field(scf_field) values ("biological sciences");
-insert into tbl_scientific_field(scf_field) values ("social sciences");
-insert into tbl_scientific_field(scf_field) values ("humanities");
-insert into tbl_scientific_field(scf_field) values ("engineering sciences");
